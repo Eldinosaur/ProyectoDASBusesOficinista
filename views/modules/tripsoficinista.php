e2786591978014e6982e2041d27d212561e4560d
Eldinosaur <eldinosaur7u7@outlook.com>
2023-07-06 00:24:33 -0500

    PD-82 Vista Viajes

---
+++ views/modules/tripsoficinista.php
@@ -0,0 +1,19 @@
+<div style="padding-left: 30px; padding-right: 30px; padding-top: 15px;">
+<table class="table table-striped">
+<thead>
+<tr>
+    <th scope="col">Origen</th>
+    <th scope="col">Destino</th>
+    <th scope="col">Hora de Salida</th>
+    <th scope="col">Duracion Viaje</th>
+</tr>
+</thead>
+  <tbody>
+    <tr>
+        <td colspan="3"><center>No existen frecuencias registradas</center></td>
+    </tr>
+    
+</tbody>
+</table>
+
+</div>
