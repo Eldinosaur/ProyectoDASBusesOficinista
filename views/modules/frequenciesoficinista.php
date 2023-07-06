d456d0502f4c8e57d19c8820b696bcc16e8ec5d3
Eldinosaur <eldinosaur7u7@outlook.com>
2023-07-06 12:48:06 -0500

    correccion de icono

--- views/modules/frequenciesoficinista.php
+++ views/modules/frequenciesoficinista.php
@@ -49,9 +49,9 @@
                                 echo 'Inactivo';
                             } ?>
                         </td>
-                        <td><a class="nav-link active; navTemplate"
+                        <td><a class="nav-link active; navTemplate" title="Editar Frecuencia"
                                 href="redireccionoficinista.php?action=updatefrequency&id_frecuencia=<?php echo $id_frecuencia;?>&origen_frecuencia=<?php echo $origen_frcuencia;?>&destino_frecuencia=<?php echo $destino_frecuencia?>&duracion_frecuencia=<?php echo $duracion_frecuencia?>&tipo_frecuencia=<?php echo $tipo_frecuencia?>&costo_frecuencia=<?php echo $costo_frecuencia?>&estado_frecuencia=<?php echo $estado_frecuencia?>">
-                                <img src="img/editar.png" class="icons">
+                                <img src="img/edit.png" class="icons">
                             </a></td>
                     </tr>
                     <?php
